85f055397301f61a082fc6d385fac2618065cbdc
Mounika1195 <Mounika1195@outlook.com>
2026-07-08 17:58:39 +0530

    Updated config reader to support GCS paths

--- dataflow/utils/config_reader.py
+++ dataflow/utils/config_reader.py
@@ -1,5 +1,4 @@
-from pathlib import Path
-
+from apache_beam.io.filesystems import FileSystems
 import yaml
 
 from dataflow.utils.logger import get_logger
@@ -10,17 +9,12 @@ logger = get_logger(__name__)
 
 def read_yaml_file(file_path):
     """
-    Reads a YAML file and returns the content as a Python dictionary.
+    Reads a YAML file from local path or GCS path and returns dictionary.
     """
     try:
-        path = Path(file_path)
-
-        if not path.exists():
-            raise FileNotFoundError(f"Config file not found: {file_path}")
-
         logger.info("Reading YAML file: %s", file_path)
 
-        with open(path, "r", encoding="utf-8") as file:
+        with FileSystems.open(file_path) as file:
             data = yaml.safe_load(file)
 
         logger.info("YAML file loaded successfully: %s", file_path)
