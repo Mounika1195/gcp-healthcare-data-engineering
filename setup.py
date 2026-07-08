83f39ba106b31b10029114f5db54ac35dcff19f9
Mounika1195 <Mounika1195@outlook.com>
2026-07-08 17:24:10 +0530

    Added setup file content

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="healthcare-dataflow-pipeline",
+    version="1.0.0",
+    description="Healthcare batch and streaming Dataflow pipelines",
+    packages=find_packages(),
+)
+
